0b14247b7124aa61542b99d68d83963faffa8e92
Sohee Kim <sohee.kim@circle.com>
2023-10-03 21:56:28 +0000

    fix solana.js

--- solana.js
+++ solana.js
@@ -5,48 +5,47 @@ const {
     clusterApiUrl,
 } = require("@solana/web3.js");
 const { getOrCreateAssociatedTokenAccount, transfer } = require("@solana/spl-token");
-const bs58 = require("bs58");
 
-const SOLANA_USDC_DEV_PUBKEY = "4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU"; // USDC TokenAddress in devnet
-const FROM_SECRET_KEY_BYTES = [];
-const TO_ADDRESS = "";
+const USDC_DEV_PUBLIC_KEY = "4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU"; // USDC TokenAddress in devnet
+const SENDER_PRIVATE_KEY_BYTES = [];
+const RECEIVER_PUBLIC_KEY = "";
 
 (async () => {
 
     const connection = new Connection(
-        clusterApiUrl('devnet'),
-        'confirmed'
+        clusterApiUrl("devnet"),
+        "confirmed",
     );
 
-    const usdcPublicKey = new PublicKey(SOLANA_USDC_DEV_PUBKEY);
-    const toAddressPublicKey = new PublicKey(TO_ADDRESS);
-    const fromSecretKey = Uint8Array.from(FROM_SECRET_KEY_BYTES);
-    const fromKeypair = Keypair.fromSecretKey(fromSecretKey);
+    const usdcPublicKey = new PublicKey(USDC_DEV_PUBLIC_KEY);
+    const receiverAddressPublicKey = new PublicKey(RECEIVER_PUBLIC_KEY);
 
+    const senderPrivateKey = Uint8Array.from(SENDER_PRIVATE_KEY_BYTES);
+    const senderKeypair = Keypair.fromSecretKey(senderPrivateKey);
 
-    const fromTokenAccount = await getOrCreateAssociatedTokenAccount(
+    const senderTokenAccount = await getOrCreateAssociatedTokenAccount(
         connection,
-        fromKeypair,
+        senderKeypair,
         usdcPublicKey,
-        fromKeypair.publicKey
+        senderKeypair.publicKey,
     );
 
-    const toTokenAccount = await getOrCreateAssociatedTokenAccount(
+    const receiverTokenAccount = await getOrCreateAssociatedTokenAccount(
         connection,
-        fromKeypair,
+        senderKeypair,
         usdcPublicKey,
-        toAddressPublicKey
+        receiverAddressPublicKey,
     );
 
-    signature = await transfer(
+    const signature = await transfer(
         connection,
-        fromKeypair,
-        fromTokenAccount.address,
-        toTokenAccount.address,
-        fromKeypair.publicKey,
-        10 // amount
+        senderKeypair,
+        senderTokenAccount.address,
+        receiverTokenAccount.address,
+        senderKeypair.publicKey,
+        10, // amount
     );
 
-    console.log('SIGNATURE:', signature);
+    console.log(`You can verify the transaction on https://explorer.solana.com/tx/${signature}?cluster=devnet`);
 
 })();
